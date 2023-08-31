39129aafc1297df7a4c5c0587de5444f1e0b002b
Nayeem Ahmad Dar <Nayeemdar3@gmail.com>
2023-08-31 03:10:45 -0700

    update new

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "2baa81c12e3f1ebf"; }
+/******/ 		__webpack_require__.h = function() { return "d80065a9d2d43611"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
